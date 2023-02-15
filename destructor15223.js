6c80d104514289876c5e6a426bd535bb3c205f32
vskrishna0522 <120077783+vskrishna0522@users.noreply.github.com>
2023-02-15 17:52:21 +0530

    Add files via upload

---
+++ destructor15223.js
@@ -0,0 +1,14 @@
+let emp = {
+
+    id : 101,
+    name: "kittu",
+    salary: 45333,
+    place: "salem",
+    details: {
+        location: "lalll",
+        place: "erode",
+    }
+}
+let {name, details} = emp
+console.log(name)
+console.log(details.location)
