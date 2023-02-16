6ba81c264c7617d7a05528f08bc56ab7ca0e4333
vskrishna0522 <120077783+vskrishna0522@users.noreply.github.com>
2023-02-16 16:19:34 +0530

    Add files via upload

---
+++ asynk.js
@@ -0,0 +1,12 @@
+setTimeout(() => {
+    console.log("kittu")
+}
+    ,3000);
+setTimeout(() => {
+        console.log("boo")
+    }
+    ,5000);
+setTimeout(() => {
+        console.log("srina")
+    }
+    ,1000);
