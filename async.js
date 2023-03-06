7bf00dd3a014dfe992dc2b70948faaad177bb296
vskrishna0522 <120077783+vskrishna0522@users.noreply.github.com>
2023-03-06 23:02:58 +0530

    Add files via upload

---
+++ async.js
@@ -0,0 +1,11 @@
+let employees = [
+    { id: 101, name: "Rahul Gandhi", sal: 45000 },
+    { id: 102, name: "Sonia Gandhi", sal: 55000 }
+]
+
+let createEmployee = () => { }
+
+let getEmployees = () => { }
+
+createEmployee()
+getEmployees()
