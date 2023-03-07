53e05fab1e56698c6926ba2ea00ad040a2756764
vskrishna0522 <120077783+vskrishna0522@users.noreply.github.com>
2023-03-07 20:01:21 +0530

    Add files via upload

---
+++ object7323.js
@@ -0,0 +1,11 @@
+let employee = {
+    id :101,
+    name : "kittu",
+    last_name : "v",
+    email : "kittu@gmail.com",
+    loc : "bengaluru",
+    role : "sr manager"
+}
+for (key in employee){
+    console.log(employee[key])
+}
